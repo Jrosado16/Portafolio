2e59a8e52e3c84f1914ea9775f5a97bd55a2a2ba
Carlos Rosado <krlos_1594@yahoo.com>
2021-01-22 16:06:31 -0600

    index init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/src/public'))
+app.use(express.static(__dirname + '/'))
 
 app.get('/', (req, res) => {
     res.write('Esperar')
--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,30 @@ const sass        = require('gulp-sass');
 gulp.task('sass', () => {
     return gulp.src(['src/assets/scss/*.scss'])
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest("src/public/css"))
+        .pipe(gulp.dest("public/css"))
         .pipe(browserSync.stream());
 });
 
 // Compile js
 gulp.task('js', () => {
     return gulp.src(['src/assets/js/*.js'])
-    .pipe(gulp.dest('src/public/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(browserSync.stream())
   });
 
-
-
-// server / watch ['sass', 'js']
 gulp.task('serve', gulp.series('sass', function() {
-
     browserSync.init({
-        server: "src/public"  
-    });
+        server: {
+            baseDir: './'
+        } 
 
+    });
     gulp.watch([
         'src/assets/scss/*.scss',
         'src/assets/js/*.js',
     ],  gulp.parallel( ['sass', 'js'] ));
 
-    gulp.watch("./src/public/*.html").on('change', browserSync.reload);
+    gulp.watch("/*.html").on('change', browserSync.reload);
 }));
 
 gulp.task('default', gulp.parallel('serve'));
